8679922b2d01b1657afff5d4b2609468656a6188
hehelihaile <hhh@126.com>
2020-11-07 16:43:35 +0800

    by idea test

---
+++ idea_git/src/test_package01/FirstClass.java
@@ -0,0 +1,11 @@
+package test_package01;
+
+/**
+ * @author xuzefei
+ * @create 2020-11-07-16:29
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
